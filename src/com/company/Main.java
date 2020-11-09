23be10dd4b7874b726f8a88c4d49ed2b492158ae
SubhankarHalder <subhankarvirgoan@gmail.com>
2020-11-09 07:56:11 +0530

    Add initial menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void menu() {
+        System.out.println("Main Menu");
+        System.out.println("\t 1. Manage Contacts");
+        System.out.println("\t 2. Messages");
+        System.out.println("\t 3. Quit");
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner console = new Scanner(System.in);
+        int choice = 0;
+        while (choice != 3) {
+            menu();
+            choice = console.nextInt();
+        }
     }
 }
